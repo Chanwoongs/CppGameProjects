af35553359ab169a798ee484dd0a203faa96ab43
Chanwoong Bang <tkfkdcksdnd@daum.net>
2025-01-02 16:46:29 +0900

    feat : cpu 타이머를 이용하여 더욱 정밀한 FPS 측정

--- Engine/ConsoleEngine/Engine/Engine.cpp
+++ Engine/ConsoleEngine/Engine/Engine.cpp
@@ -15,8 +15,20 @@ Engine::~Engine()
 void Engine::Run()
 {
 	// 시작 타임 스탬프
-	unsigned long currentTime = timeGetTime();
-	unsigned long previousTIme = 0;
+	//unsigned long currentTime = timeGetTime();
+	//unsigned long previousTIme = 0;
+
+	// CPU 
+	LARGE_INTEGER frequency;
+	QueryPerformanceFrequency(&frequency); // CPU가 가지고 있는 hz 반환
+
+	// 시작 시간 및 이전 시간을 위한 변수
+	LARGE_INTEGER time;
+	QueryPerformanceCounter(&time);
+
+	// long long == __int64 == int64_t
+	int64_t currentTime = time.QuadPart; // 64bit 한 번에 저장
+	int64_t previousTime = 0;
 
 	// 프레임 제한
 	float targetFrameRate = 60.0f;
@@ -33,10 +45,13 @@ void Engine::Run()
 		}
 
 		// 현재 프레임 시간 저장
-		currentTime = timeGetTime();
+		// currentTime = timeGetTime();
+		QueryPerformanceCounter(&time);
+		currentTime = time.QuadPart;
 
 		// 프레임 시간 계산
-		float deltaTime = static_cast<float>(currentTime - previousTIme) / 1000.0f;
+		float deltaTime = static_cast<float>(currentTime - previousTime) / 
+			static_cast<float>(frequency.QuadPart);
 
 		// 프레임 확인
 		if (deltaTime >= targetOneFrameTime)
@@ -46,7 +61,7 @@ void Engine::Run()
 			Draw();
 
 			// 이전 프레임 시간 저장
-			previousTIme = currentTime;
+			previousTime = currentTime;
 		}
 	}
 }
