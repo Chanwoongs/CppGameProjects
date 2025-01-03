d5e3bb0831b83e7ac9539ea5a7ca9c8fd3466fd1
Chanwoong Bang <tkfkdcksdnd@daum.net>
2025-01-03 14:19:59 +0900

    feat : 동적 배열 생성

--- Engine/ConsoleEngine/Level/Level.cpp
+++ Engine/ConsoleEngine/Level/Level.cpp
@@ -34,4 +34,31 @@ void Level::Draw()
 
 void Level::AddActor(Actor* newActor)
 {
+	// 현재 할당된 공간이 충분한지 확인
+	if (count == capacity)
+	{
+		// 공간 추가 할당
+		int newCapacity = capacity * 2;
+
+		// 임시 공간
+		Actor** temp = new Actor*[newCapacity];
+
+		// 기존 값 복사
+		memcpy(temp, actors, sizeof(size_t) * capacity);
+
+		// 기존 값 삭제
+		delete[] actors;
+
+		// 배열 교체
+		actors = temp;
+
+		// 크기 변경
+		capacity = newCapacity;
+
+		// temp는 로컬 변수이기 때문에 자동으로 사라짐
+	}
+
+	// 액터 추가
+	actors[count] = newActor;
+	++count;
 }
