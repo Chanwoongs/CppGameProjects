be5e084a9f33bea67d0596d8b591cf97bcd88034
Chanwoong Bang <tkfkdcksdnd@daum.net>
2025-01-03 15:14:10 +0900

    delete : Test Level 삭제

--- Engine/ConsoleEngine/main.cpp
+++ Engine/ConsoleEngine/main.cpp
@@ -1,7 +1,6 @@
 ﻿#include <iostream>
 
 #include "Engine/Engine.h"
-#include "TestLevel.h"
 
 int main()
 {
@@ -11,6 +10,5 @@ int main()
 	// 로그 시스템 초기화
 	//////////////////////
 
-	engine.LoadLevel(new TestLevel());
 	engine.Run();
 }
