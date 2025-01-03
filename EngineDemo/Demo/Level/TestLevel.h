252c7d46419b01ff775f4ad6c11f7a7799f3d414
Chanwoong Bang <tkfkdcksdnd@daum.net>
2025-01-03 16:29:50 +0900

    feat : lib 파일을 생성하여 dll 파일 연결

---
+++ EngineDemo/Demo/Level/TestLevel.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "Level/Level.h"
+
+class TestLevel : public Level
+{
+public:
+	TestLevel();
+	~TestLevel();
+
+	void Update(float deltaTime);
+
+private:
+
+};
--- EngineDemo/Demo/main.cpp
+++ EngineDemo/Demo/main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 
 #include "Game/Game.h"
+#include "Level/TestLevel.h"
 
 int main()
 {
 	Game game;
+	game.LoadLevel(new TestLevel());
 	game.Run();
 }
