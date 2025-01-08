772ca2464efe3cf91d9ac02294ce8a0675560fa9
Chanwoong Bang <tkfkdcksdnd@daum.net>
2025-01-08 12:41:09 +0900

    feat : Demo 프로젝트를 Engine 프로젝트로 옮겨서 dll 자동화

---
+++ Engine/Demo/Level/TestLevel.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+
+#include "Level/Level.h"
+
+class TestLevel : public Level
+{
+    // RTTI
+    RTTI_DECLARATIONS(TestLevel, Level)
+
+public:
+	TestLevel();
+	~TestLevel();
+
+	virtual void Update(float deltaTime) override;
+
+private:
+
+};
