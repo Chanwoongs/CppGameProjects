99e69cffe726fc6e71d5a0a4207b1f5f61005248
Chanwoong Bang <tkfkdcksdnd@daum.net>
2025-01-15 12:18:47 +0900

    add: sokoban 게임 프로젝트 생성

---
+++ SokobanGame/ConsoleEngine/Level/Level.cpp
@@ -0,0 +1,81 @@
+﻿#include "PrecompiledHeader.h"
+
+#include "Level.h"
+#include "Actor/Actor.h"
+
+Level::Level()
+{
+}
+
+Level::~Level()
+{
+	// 메모리 해제
+    for (Actor* actor : actors)
+    {
+        delete actor;
+    }
+}
+
+void Level::Update(float deltaTime)
+{
+	// 레벨에 포함된 액터를 순회하면서 Update 함수 호출
+    for (Actor* actor : actors)
+    {
+        if (!actor->isActive || actor->isExpired)
+        {
+            continue;
+        }
+        actor->Update(deltaTime);
+    }
+}
+
+void Level::Draw()
+{	
+	// 레벨에 포함된 액터를 순회하면서 Draw 함수 호출
+    for (Actor* actor : actors)
+    {
+        actor->Draw();
+    }
+}
+
+void Level::AddActor(Actor* newActor)
+{
+    //actors.PushBack(newActor);
+    addRequestedActor = newActor;
+}
+
+void Level::ProcessAddedAndDestroyActor()
+{
+    // 삭제 요청된 액터 처리
+    for (int i = 0; i < actors.Size();)
+    {
+        if (actors[i]->isExpired)
+        {
+            delete actors[i];
+            actors[i] = nullptr;
+            actors.Erase(i);
+        }
+        ++i;
+    }
+
+    // 추가 요청된 액터 처리
+    if (addRequestedActor)
+    {
+        actors.PushBack(addRequestedActor);
+        addRequestedActor = nullptr;
+    }
+}
+
+//void Level::DestroyActor()
+//{
+//    for (int i = 0; i < actors.Size();)
+//    {
+//        if (actors[i]->isExpired)
+//        {
+//            delete actors[i];
+//            actors[i] = nullptr;
+//            actors.Erase(i);
+//        }
+//        ++i;
+//    }
+//}
---
+++ SokobanGame/ConsoleEngine/PrecompiledHeader/PrecompiledHeader.h
@@ -0,0 +1,9 @@
+﻿#pragma once
+
+// pch를 사용하면
+// 빌드 프로세스의 속도를 높임, 시간 단축, 컴파일 시간이 오래 걸린다.
+// Header를 하나 정해서 컴파일을 미리 해놓고, 컴파일 된 헤더를 가져다 쓴다.
+
+// 미리 컴파일된 헤더
+#include <iostream>
+#include <Windows.h>
